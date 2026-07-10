39b490a14e6e972ee7bd30796f247f9cbbab69be
Talha Shaikh <Talha369852@gmail.com>
2026-07-10 22:02:39 +0500

    add number guessing game project

---
+++ projects/number-guessing-game/index.ts
@@ -0,0 +1,18 @@
+import inquirer from "inquirer"
+
+let computerNum: number = Math.floor(Math.random() * 10 + 1 )
+
+let userNum = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: "Guess a no 1 - 10: "
+    }
+])
+
+if (userNum.userGuessNumber === computerNum) {
+    console.log("You won");
+    
+} else {
+    console.log("You loose guess Wrong no");
+}
